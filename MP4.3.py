765d5cada02447ff5b189edacf5fafea4a19829e
nov <jeremyvon21@gmail.com>
2026-06-22 23:24:39 +0800

    Update MP4.3.py

--- MP4.3.py
+++ MP4.3.py
@@ -1,3 +1,7 @@
+#Name : Jeremy Von San Juan
+#School : FEU TECH
+#MACHINE PROBLEM NUMBER : 3
+
 class converttoroman:
     def __init__(self):
         self.numbers = [(5000, 'MMMMM'), (4000, 'MMMM'), (3000, 'MMM'), (2000, 'MM'), (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
